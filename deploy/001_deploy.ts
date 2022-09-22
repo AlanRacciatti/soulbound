da0d9bb365f1a6f6360dad47dce0dc38fd9f472d
AlanRacciatti <alanracciatti1220@gmail.com>
2022-09-22 17:34:32 -0300

    feat: deploy script

--- deploy/001_deploy.ts
+++ deploy/001_deploy.ts
@@ -4,16 +4,19 @@ import { getChainId, shouldVerifyContract } from '../utils/deploy';
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
+  const args = ['ipfs://QmX24rdvjNeHonXuhLirRqmppQyY5KKPApDd7Bm2Lp9HyN', 'ipfs://QmbM8Q7KqByyECd1xvXTgGSvry2SoiYjKWDMjA4UqDsoNZ/'];
 
   const deploy = await hre.deployments.deploy('SBDToken', {
     contract: 'solidity/contracts/SBDToken.sol:SBDToken',
     from: deployer,
+    args,
     log: true,
   });
 
   if (await shouldVerifyContract(deploy)) {
     await hre.run('verify:verify', {
       address: deploy.address,
+      constructorArguments: args,
     });
   }
 };
